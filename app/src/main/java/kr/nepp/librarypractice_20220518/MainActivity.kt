04be4cf4ac8b888e60b17dfe4047cdff2f265f2d
조경진 <cho881020@gmail.com>
2022-05-18 15:22:23 +0900

    권한 여부에 따른 행동방침만 작성 (실제 권한 확인 X)

--- app/src/main/java/kr/nepp/librarypractice_20220518/MainActivity.kt
+++ app/src/main/java/kr/nepp/librarypractice_20220518/MainActivity.kt
@@ -1,8 +1,11 @@
 package kr.nepp.librarypractice_20220518
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +22,29 @@ class MainActivity : AppCompatActivity() {
 
 //            Intent (4) - CALL : 권한이 없어서 앱이 죽는다.
 
-//            권한이 있는지? 확인부터 해보자. => 권한이 있어야만 CALL 실행.
+//            1. 권한 여부에 따른 행동 방침 - 인터페이스 변수
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+//                    실제 CALL 액션 실행.
+
+                    val myUri = Uri.parse("tel:01055556666")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@MainActivity, "권한이 없어서 통화가 불가능 합니다.", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+
+            }
+
+//            2. 권한 확인 요청
 
         }
 
